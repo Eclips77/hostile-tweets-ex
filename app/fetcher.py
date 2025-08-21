4dea3ba2fa23ed98d967e17665d5ab215a90f543
Eclipse <Eclips77@users.noreply.github.com>
2025-08-21 09:42:56 +0300

    Add FetcherDal class for MongoDB data access and update config for collection name

---
+++ app/fetcher.py
@@ -0,0 +1,13 @@
+from app.connection import DatabaseConnection
+from typing import List, Optional
+from data import config
+
+class FetcherDal:
+    """Data Access Layer for fetching tweet data from mongodb ."""
+    def __init__(self):
+        """ 
+        Initialize DAL with database connection
+        """
+        self.connection = DatabaseConnection()
+        self.database = self.connection.connect()
+        self.collection = self.database[config.MONGODB_COLLECTION]
