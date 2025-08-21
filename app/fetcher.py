d3f91064a8a84b37aa69aa9da57ebefa49f98920
Eclipse <Eclips77@users.noreply.github.com>
2025-08-21 10:03:26 +0300

    Implement fetch_all_tweets method in FetcherDal class for retrieving tweets from MongoDB

--- app/fetcher.py
+++ app/fetcher.py
@@ -1,6 +1,7 @@
 from app.connection import DatabaseConnection
 from typing import List, Optional
 from data import config
+import logging
 
 class FetcherDal:
     """Data Access Layer for fetching tweet data from mongodb ."""
@@ -13,4 +14,18 @@ class FetcherDal:
         self.collection = self.database[config.MONGODB_COLLECTION]
     
     def fetch_all_tweets(self):
-        pass
+        """
+        Get all tweets from the database
+        
+        Returns:
+            List of all tweets
+        """
+        try:
+            results = self.collection.find({},{"_id":0})
+            return [doc for doc in results]
+        except Exception as e:
+            print(f"Error getting all tweets: {e}")
+            return []
+
+
+     
