ba3d10a9ade17bdcbe121273f5ad9c92d96d1084
Eclipse <Eclips77@users.noreply.github.com>
2025-08-21 09:33:18 +0300

    Refactor config.py to use environment variables for sensitive data

--- data/config.py
+++ data/config.py
@@ -1,4 +1,6 @@
-USER = "IRGC"
-PASSWORD = "iraniraniran"
-DB_NAME = "IranMalDB"
-CONNECTION_STRING = "mongodb+srv://IRGC:iraniraniran@iranmaldb.gurutam.mongodb.net/"
+import os
+
+USER = os.getenv("MONGO_USER", "IRGC")
+PASSWORD = os.getenv("MONGO_PASSWORD", "iraniraniran")
+DB_NAME = os.getenv("MONGO_DB_NAME", "IranMalDB")
+CONNECTION_STRING = os.getenv("MONGO_CONNECTION_STRING", "mongodb+srv://IRGC:iraniraniran@iranmaldb.gurutam.mongodb.net/")
